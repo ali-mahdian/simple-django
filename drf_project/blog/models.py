1dfdf6e28935c418f5f1322da3cbda1117833711
Ali Mahdian <a.mahdian@digikala.com>
2023-11-27 18:33:12 +0330

    add Post and Rating models

--- drf_project/blog/models.py
+++ drf_project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings')
+    value = models.IntegerField(choices=[(i, i) for i in range(6)])
